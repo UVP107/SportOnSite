f9c57cc6a1658ddc830dfa265dc2930dc188adb2
UVP107 <unggulvalentinoputra3@gmail.com>
2026-01-15 11:07:08 +0700

    first commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>halo apakah ini poopins?</h1>
+    </main>
+  );
+}
